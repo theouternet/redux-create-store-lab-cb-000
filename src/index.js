14c7cbf22ec22dec8c27cc2adc371b9272b836d4
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-06-28 22:14:03 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -20,3 +20,47 @@ function render() {
 
 // use your createStore function and the functions provided here to create a store
 // once the store is created, call an initial dispatch
+
+---
+function createStore(reducer) {
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+
+  function getState() {
+    return state;
+  };
+
+  return {
+    dispatch,
+    getState
+  };
+};
+
+function reducer(state = { count: 0 }, action) {
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return { count: state.count + 1 };
+
+    default:
+      return state;
+  }
+}
+
+
+function render() {
+  let container = document.getElementById('container');
+  container.textContent = store.getState().count;
+};
+
+let store = createStore(reducer) 
+
+store.dispatch({ type: '@@INIT' });
+let button = document.getElementById('button');
+
+button.addEventListener('click', function() {
+  store.dispatch({ type: 'INCREASE_COUNT' });
+});
